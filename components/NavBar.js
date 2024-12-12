d86d40452e111654f17887e37f500b87bc42da71
moor <taimoor725tanveer@gmail.com>
2024-12-12 09:37:04 +0500

    Services tab corrected
    icon added/font has been adjust

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import Link from "next/link";
 import ServicesHandler from './ServicesHandler';
+import Image from 'next/image';
 
 
 
@@ -10,15 +11,17 @@ function NavBar() {
   const [isVisible, setVisible] = useState(false);
 
   return (
-    <div className="fixed top-8 right-0 lg:right-10 flex items-center justify-center gap-4 lg:w-auto w-full text-base lg:text-2xl z-50">
+    <div className="fixed top-8 right-0 lg:right-10 flex items-center justify-center gap-6 lg:w-auto w-full text-base lg:text-2xl z-50">
       <Link href={'/home'}>Home</Link>
       <Link href={'/about'}>About</Link>
-      <Link href={'/services'} onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>Services
+      <Link href={'/services'} className='flex gap-1 justify-center items-center' onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>Services
         {isVisible && (
           <ServicesHandler />
         )
         }
+      <Image src={'/icons/dropdown.svg'} width={60} height={60} className={`lg:w-6 lg:h-6 w-4 h-4 transition-all duration-200 ${isVisible ? "-rotate-180": ""}`}/>
       </Link>
+
       <Link href={'#footer'}>Contact</Link>
     </div>
   );
--- components/ServicesHandler.js
+++ components/ServicesHandler.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 function ServicesHandler() {
   return (
-    <div className='lg:w-[800px] w-auto h-auto bg-white/95 absolute top-8 lg:right-0 right-10 z-50 rounded-xl '>
+    <div className='lg:w-auto w-auto h-auto bg-white/95 absolute top-8 lg:right-0 right-10 z-50 rounded-xl '>
         <HowCanWeHelp/>
     </div>
   )
@@ -20,7 +20,7 @@ export default ServicesHandler
 const HowCanWeHelp = () => {
     return (
         <div className="flex flex-col relative font-semibold text-center gap-3">
-            <div className="flex lg:flex-row flex-col">
+            <div className="flex flex-col">
                 <NavSection ID={'one_on_one'} imgSrc={'/icons/1to1 Coaching.png'} title={"One-On-One Sessions"} detail={"Safe, inclusive coaching for neurodivergent, queer, and people of colour—personalised support for better wellbeing."} />
                 <NavSection ID={'training'} imgSrc={'/icons/Training.png'} title={"workplace training"} detail={"Bespoke guidance on mental health, neurodiversity, and inclusivity, tailored to the specific needs of each organisation—going beyond a one-size-fits-all approach."} />
                 <NavSection ID={'consultancy'} imgSrc={'/icons/Consultancy.png'} title={"Consultancy"} detail={"Unlike training, we partner with organisations to solve complex challenges, create tailored strategies, and implement lasting solutions for deeper, impactful inclusivity."} />
@@ -31,9 +31,10 @@ const HowCanWeHelp = () => {
 
 const NavSection=({imgSrc, title, detail,ID})=>{
     return (
-        <Link href={`/services#${ID}`} className={`flex flex-col items-center justify-center text-center lg:p-8 p-4 gap-4 text-pretty`}>
-            <Image src={imgSrc} width={600} height={600} className="lg:w-60 lg:h-auto _w-60 lg:block hidden" alt="bg" />
-            <span className="text-xl font-bold capitalize">{title}</span>
+        <Link href={`/services#${ID}`} className={`flex items-center justify-center text-center lg:p-8 p-4 gap-4 text-pretty`}>
+            {/* <Image src={imgSrc} width={600} height={600} className="lg:w-60 lg:h-auto _w-60 lg:block hidden" alt="bg" /> */}
+            <span className="lg:text-xl text-xs font-bold capitalize">{title}</span>
+
             {/* <span className="text-black text-sm lg:text-base font-light lg:w-4/6">{detail}</span> */}
         </Link>
     )
